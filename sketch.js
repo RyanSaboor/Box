27c94ec082287b1f0571b610e98c7ee16f237085
RyanSaboor <98928686+RyanSaboor@users.noreply.github.com>
2022-02-02 17:38:50 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var Box
+function setup() {
+  createCanvas(400,400);
+  Box = createSprite (200,200,30,30);
+}
+
+function draw() 
+{
+  background(30);
+
+  if (keyIsDown(RIGHT_ARROW)){
+    Box.position.x = Box.position.x + 5
+  }
+  if (keyIsDown(LEFT_ARROW)){
+    Box.position.x = Box.position.x - 5
+  }
+  if (keyIsDown(UP_ARROW)){
+    Box.position.y = Box.position.y - 5
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    Box.position.y = Box.position.y + 5
+  }
+  drawSprites();
+
+}
+
+
+
+
